2eb0b44358a3138bf9cf4130de186161f0cd3b96
Shengwei Wang <Shengwei.Wang@creditkarma.com>
2016-12-11 23:08:46 -0800

    new tests

--- src/test/scala/com/creditkarma/blink/test/BlinkGCSWriterIntegrationTest.scala
+++ src/test/scala/com/creditkarma/blink/test/BlinkGCSWriterIntegrationTest.scala
@@ -4,27 +4,71 @@ import com.creditkarma.blink.MainApp
 import com.creditkarma.blink.factory.KafkaExportWorkerCreator
 import com.creditkarma.blink.impl.spark.exporter.kafka.gcs.{GCSSubPartition, GCSWriter}
 import org.apache.log4j.{Level, LogManager}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest._
 
 /**
   * Created by shengwei.wang on 12/9/16.
   */
-object BlinkGCSWriterIntegrationTest extends KafkaIntegrationTest[String, String]{
+class BlinkGCSWriterIntegrationTest extends FlatSpec with KafkaIntegrationTest[String, String] {
 
+  val conf = new SparkConf().setAppName("spark gcs connector test").setMaster("local[4]")
+  val sc = new SparkContext(conf)
+
+  sc.hadoopConfiguration.set("fs.gs.project.id", "295779567055")
+  sc.hadoopConfiguration.set("fs.gs.impl","com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+  sc.hadoopConfiguration.set("google.cloud.auth.service.account.enable", "true")
+  sc.hadoopConfiguration.set("google.cloud.auth.service.account.email", "dataeng-test@modular-shard-92519.iam.gserviceaccount.com")
+  sc.hadoopConfiguration.set("google.cloud.auth.service.account.keyfile", "/Users/shengwei.wang/projects/DataScience-ac040bae47fb.p12")
   override val configuredPorts: Option[(Int, Int)] = Some((5678, 1234))
-   def main(args: Array[String]) {
+
+  "A writer" should "upload the data/message correctly" in {
 
     // LogManager.getLogger("org.apache").setLevel(Level.WARN)
    //  LogManager.getLogger("kafka").setLevel(Level.WARN)
 startKafka()
+
+    //parseables
      sendMessage("SaturdayEvening", "k1", "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"2014-05-07T14:22:35.863513-08:00\"}")
      sendMessage("SaturdayEvening", "k1", "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"2016-11-27T14:22:35.863513-08:00\"}")
      sendMessage("SaturdayEvening", "k1", "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"2019-05-07T14:22:35.863513-08:00\"}")
-     sendMessage("SaturdayEvening", "k1", "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"20196-05-07T14:22:35.863513-08:00\"}")
-     sendMessage("SaturdayEvening", "k1", "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"20a19-05-07T14:22:35.863513-08:00\"}")
-     sendMessage("SaturdayEvening", "k1", "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"tsfad\":\"2016-12-11T14:22:35.863513-08:00\"}")
      sendMessage("SaturdayEvening", "k1", "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"2016-12-12T14:22:35.863513-08:00\"}")
+    //unparseables
+    sendMessage("SaturdayEvening", "k1", "traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName08:00\"}")
+    sendMessage("SaturdayEvening", "k1", "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995ancidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"20a19-05-07T14:22:35.863513-08:00\"}")
+    sendMessage("SaturdayEvening", "k1", "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"12-11T14:22:35.863513-08:00\"")
+    sendMessage("SaturdayEvening", "k1", "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"20196-05-07T14:22:35.863513-08:00\"}")
+    sendMessage("SaturdayEvening", "k1", "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"20a19-05-07T14:22:35.863513-08:00\"}")
+    sendMessage("SaturdayEvening", "k1", "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"tsfad\":\"2016-12-11T14:22:35.863513-08:00\"}")
+
      val portalId = MainApp.castPortal("src/test/resources/kafka.test3.properties")
-shutDownKafka()
+
+    val myRDD1 = sc.textFile("gs://dataeng_test/SaturdayEvening/0/2014/05/07/0.json")
+    val local1 = myRDD1.toLocalIterator
+    assert(local1.next() == "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"2014-05-07T14:22:35.863513-08:00\"}")
+
+    val myRDD2 = sc.textFile("gs://dataeng_test/SaturdayEvening/0/2016/11/27/0.json")
+    val local2 = myRDD2.toLocalIterator
+    assert(local2.next() == "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"2016-11-27T14:22:35.863513-08:00\"}")
+
+    val myRDD3 = sc.textFile("gs://dataeng_test/SaturdayEvening/0/2019/05/07/0.json")
+    val local3 = myRDD3.toLocalIterator
+    assert(local3.next() =="{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"2019-05-07T14:22:35.863513-08:00\"}")
+
+    val myRDD4 = sc.textFile("gs://dataeng_test/SaturdayEvening/0/2016/12/12/0.json")
+    val local4 = myRDD4.toLocalIterator
+    assert(local4.next() == "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"2016-12-12T14:22:35.863513-08:00\"}")
+
+
+    val myRDD5 = sc.textFile("gs://dataeng_test/SaturdayEvening/0/1969/12/31/0.json")
+    val local5 = myRDD5.toLocalIterator
+    assert(local5.next() == "traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName08:00\"}")
+    assert(local5.next() == "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995ancidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"20a19-05-07T14:22:35.863513-08:00\"}")
+    assert(local5.next() == "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"12-11T14:22:35.863513-08:00\"")
+      assert(local5.next() == "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"20196-05-07T14:22:35.863513-08:00\"}")
+        assert(local5.next() == "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"20a19-05-07T14:22:35.863513-08:00\"}")
+          assert(local5.next() == "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"tsfad\":\"2016-12-11T14:22:35.863513-08:00\"}")
+            shutDownKafka()
 
     }
 }
--- src/test/scala/com/creditkarma/blink/test/GCSWriterTest.scala
+++ src/test/scala/com/creditkarma/blink/test/GCSWriterTest.scala
@@ -3,60 +3,76 @@ package com.creditkarma.blink.test
 import com.creditkarma.blink.impl.spark.exporter.kafka.{KafkaMessageId, KafkaMessageWithId, SubPartition, WorkerMeta}
 import com.creditkarma.blink.impl.spark.exporter.kafka.gcs.{GCSSubPartition, GCSWriter}
 import org.apache.kafka.common.TopicPartition
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.streaming.kafka010.OffsetRange
+import org.scalatest.FlatSpec
 
 import scala.collection.mutable
 
 /**
-  * Created by shengwei.wang on 12/7/16.
+  * Created by shengwei.wang on 12/11/16.
   */
-object GCSWriterUnitTests {
+class GCSWriterTest extends FlatSpec{
+  val mGCSWriter: GCSWriter = new GCSWriter(
+    "ts",
+    true,
+    "",
+    "/Users/shengwei.wang/projects/DataScience-f7d364638ad4.json",
+    10000,
+    10000,
+    "dataeng_test",
+    "application/json",
+    "priority,high;period,60",
+    "",
+    "json"
+  )
+
+  val conf = new SparkConf().setAppName("spark gcs connector test").setMaster("local[8]")
+  val sc = new SparkContext(conf)
+
+  sc.hadoopConfiguration.set("fs.gs.project.id", "295779567055")
+  sc.hadoopConfiguration.set("fs.gs.impl","com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+  sc.hadoopConfiguration.set("google.cloud.auth.service.account.enable", "true")
+  sc.hadoopConfiguration.set("google.cloud.auth.service.account.email", "dataeng-test@modular-shard-92519.iam.gserviceaccount.com")
+  sc.hadoopConfiguration.set("google.cloud.auth.service.account.keyfile", "/Users/shengwei.wang/projects/DataScience-ac040bae47fb.p12")
 
 
-  def main(args: Array[String]): Unit = {
-
-    val mGCSWriter: GCSWriter = new GCSWriter(
-      "ts",
-      true,
-      "",
-      "/Users/shengwei.wang/projects/DataScience-f7d364638ad4.json",
-      10000,
-      10000,
-      "dataeng_test",
-      "application/json",
-      "priority,high;period,60",
-      "",
-      "json"
-    )
-
+  "A writer" should "get the subpartition properly" in {
 
     val testString: String = "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"2015-01-07T14:22:35.863513-08:00\"}"
     val mGCSSubPartition: GCSSubPartition = mGCSWriter.getSubPartition(testString)
-    println(mGCSSubPartition.timePartitionPath)
+    assert(mGCSSubPartition.timePartitionPath == "2015/01/07")
 
     val testString2: String = "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-00T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"1991-02-08T14:22:35.8613-0800\"}"
     val mGCSSubPartition2: GCSSubPartition = mGCSWriter.getSubPartition(testString2)
-    println(mGCSSubPartition2.timePartitionPath)
+    assert(mGCSSubPartition2.timePartitionPath == "1991/02/08")
 
     var testString3: String = "{\"dwNumericId\":5446427592603205633,\"ts\":\"1994-01-07T14:22:35.8613-0800\"}"
     val mGCSSubPartition3: GCSSubPartition = mGCSWriter.getSubPartition(testString3)
-    println(mGCSSubPartition3.timePartitionPath)
+    assert(mGCSSubPartition3.timePartitionPath == "1994/01/07")
 
-    val testTopicPartition: TopicPartition = new TopicPartition("Night", 5)
+  }
+
+
+  "A writer" should "upload the data/message correctly" in {
+
+    val testTopicPartition: TopicPartition = new TopicPartition("GCSWriterUnitTest", 5)
     val fakeKMI = KafkaMessageId(testTopicPartition,1)
     val data: mutable.MutableList[KafkaMessageWithId[String, String]] = new mutable.MutableList[KafkaMessageWithId[String, String]]
     val payload1 = "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-01T00:00:00-0800\",\"incidentType\":\"MINOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"2015-11-27T14:22:35.863513-08:00\"}"
-    val payload2 = "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-01T00:00:00-0800\",\"incidentType\":\"MAJOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"2015-11-17T14:22:35.863513-08:00\"}"
+    val payload2 = "{\"dwNumericId\":5446427592603205633,\"traceId\":\"3a47bfd7-f050-424a-8bb3-1e84a3ab995a\",\"policyHolderId\":73,\"accidentIdx\":1,\"yearMonth\":\"2015-03-01T00:00:00-0800\",\"incidentType\":\"MAJOR\",\"tsEvent\":\"1453492695.320\",\"driverIdx\":1,\"source\":\"web014.be.prod.iad1.ckint.io\",\"schemaName\":\"Accident.json\",\"version\":\"3c4eb462888500d9f161b4d637123e72\",\"ts\":\"2015-11-27T14:22:35.863513-08:00\"}"
     data += new KafkaMessageWithId[String,String]("testKey", payload1, fakeKMI)
     data += new KafkaMessageWithId[String,String]("testKey", payload2, fakeKMI)
-    data += new KafkaMessageWithId[String,String]("testKey", "test 2nd line", fakeKMI)
-    data += new KafkaMessageWithId[String,String]("testKey", "test 3rd line", fakeKMI)
-    data += new KafkaMessageWithId[String,String]("testKey", "test 4th line", fakeKMI)
-    val subPartition = SubPartition(OffsetRange(testTopicPartition, 1234567, 2345678), Some(mGCSWriter.getSubPartition("BAD PAYLOAD")))
+    val subPartition = SubPartition(OffsetRange(testTopicPartition, 1234567, 2345678), Some(mGCSWriter.getSubPartition(payload1)))
     val result: WorkerMeta = mGCSWriter.write(subPartition, data.toIterator)
 
-    println(result.records + " " + result.bytes + " " + result.complete)
 
-
+    val myRDD = sc.textFile("gs://dataeng_test/GCSWriterUnitTest/5/2015/11/27/1234567.json")
+    assert(result.records == myRDD.count())
+    assert(result.complete == true)
+    val localIterator = myRDD.toLocalIterator
+    assert(localIterator.next() == payload1)
+    assert(localIterator.next() == payload2)
   }
+
 }
