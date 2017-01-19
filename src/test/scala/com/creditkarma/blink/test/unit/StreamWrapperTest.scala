2953abd83fc6033dda3a968bcefc6339f73c4aa2
Yongjia Wang <Yongjia.Wang@creditkarma.com>
2017-01-19 06:45:09 -0800

    consolidate and improve tests

--- src/test/scala/com/creditkarma/blink/test/unit/StreamWrapperTest.scala
+++ src/test/scala/com/creditkarma/blink/test/unit/StreamWrapperTest.scala
@@ -1,6 +1,6 @@
 package com.creditkarma.blink.test.unit
 
-import java.io.{ByteArrayOutputStream, FileInputStream, InputStream}
+import java.io._
 import java.util.zip.GZIPInputStream
 
 import com.creditkarma.blink.utils.StreamWrapper
@@ -13,15 +13,16 @@ import org.scalatest.WordSpec
   */
 class StreamWrapperTest extends WordSpec {
 
-  def sampleFile: String = "README.md"
-  def byteIteratorStream: Iterator[Byte] = {
-    IOUtils.toByteArray(new FileInputStream(sampleFile)).iterator
-  }
 
-  def compressedInputStream: InputStream = StreamWrapper(byteIteratorStream, classOf[GzipCodec])
-  val out1 = new ByteArrayOutputStream()
-  IOUtils.copyLarge(new FileInputStream(sampleFile), out1)
   "A compressed stream" must {
+    def sampleFile: String = "README.md"
+    def byteIteratorStream: Iterator[Byte] = {
+      IOUtils.toByteArray(new FileInputStream(sampleFile)).iterator
+    }
+
+    def compressedInputStream: InputStream = StreamWrapper(byteIteratorStream, classOf[GzipCodec])
+    val out1 = new ByteArrayOutputStream()
+    IOUtils.copyLarge(new FileInputStream(sampleFile), out1)
 
     "have fewer bytes" in {
       val out2 = new ByteArrayOutputStream()
@@ -35,4 +36,31 @@ class StreamWrapperTest extends WordSpec {
       assert(out1.toByteArray.toSeq == out2.toByteArray.toSeq)
     }
   }
+
+
+  "A string iterator stream" must {
+    val sampleInput = Seq("aaa", "bbb", "ccc")
+
+    "have identical bytes in plain stream" in {
+      // must explicitly append new line separator to each of the elements
+      def plainStream: InputStream = StreamWrapper(sampleInput.iterator.map(_ + "\n"), None, "UTF-8")
+      val plainStreamReader = new BufferedReader(new InputStreamReader(plainStream))
+      for(line <- sampleInput){
+        val readLine = plainStreamReader.readLine()
+        assert(readLine == line)
+      }
+    }
+
+    "have identical bytes after compression/decompression" in {
+      // must explicitly append new line separator to each of the elements
+      def compressedInputStream: InputStream = StreamWrapper(sampleInput.iterator.map(_ + "\n"), Some(classOf[GzipCodec]), "UTF-8")
+      val decompressedStreamReader = new BufferedReader(new InputStreamReader(new GZIPInputStream(compressedInputStream)))
+      for(line <- sampleInput){
+        val readLine = decompressedStreamReader.readLine()
+        assert(readLine == line)
+      }
+    }
+  }
+
+
 }
